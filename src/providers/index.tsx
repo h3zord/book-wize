8f39db1390d65fbe2c022522a67d1406a750d030
Lucas <lucaschmain@gmail.com>
2024-03-25 16:20:27 -0300

    auth: config google and github auth

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,14 +4,17 @@ import StyledComponentsRegistry from '../lib/registry'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from '../styles/global'
 import { defaultTheme } from '@/styles/theme'
+import { SessionProvider } from 'next-auth/react'
 
 export const Providers = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <StyledComponentsRegistry>
-      <GlobalStyles />
-      <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
-    </StyledComponentsRegistry>
+    <SessionProvider>
+      <StyledComponentsRegistry>
+        <GlobalStyles />
+        <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+      </StyledComponentsRegistry>
+    </SessionProvider>
   )
 }
