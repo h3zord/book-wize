94b454154a10939d7adf5a3d50c6a5bdda2f542b
Lucas <lucaschmain@gmail.com>
2024-06-28 11:01:54 -0300

    deploy

--- src/app/(book-wize)/login/index.tsx
+++ src/app/(book-wize)/login/index.tsx
@@ -1,27 +1,28 @@
-// import Image from 'next/image'
-import { SignInOptions } from '../../components/sign-in-options'
-import { LoginContainer, SignInContainer, WelcomeText } from './styles'
+import Image from 'next/image'
+import { LoginContainer } from './styles'
+// import { SignInOptions } from '../../components/sign-in-options'
+// import { LoginContainer, SignInContainer, WelcomeText } from './styles'
 
 export default function Login() {
   return (
     <LoginContainer>
-      {/* <Image
+      <Image
         src="/login-logo.svg"
         alt="Book wize logo"
         width={598}
         height={912}
         quality={100}
         priority
-      /> */}
+      />
 
-      <SignInContainer>
+      {/* <SignInContainer>
         <WelcomeText>
           <h2>Boas vindas!</h2>
           <p>Faça seu login ou acesse como visitante.</p>
         </WelcomeText>
 
         <SignInOptions />
-      </SignInContainer>
+      </SignInContainer> */}
     </LoginContainer>
   )
 }
